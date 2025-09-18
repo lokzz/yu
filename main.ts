5aa32aaa2d17bf506555a742fe3ab5f485a5b1e3
lokzz <lokzz@lokzz.uk.to>
2025-09-18 21:57:53 +0800

    changed something

--- main.ts
+++ main.ts
@@ -11,13 +11,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     } else {
         spawn_delay = 0
     }
-    delaySprite.sayText("new delay: " + [
-    0,
-    100,
-    150,
-    200,
-    250
-    ][spawn_delay], 1000)
+    delaySprite.sayText("new delay: " + spawn_delay_trans[spawn_delay], 1000)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     control_handler(way_left)
@@ -28,7 +22,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     control_handler(way_down)
 })
-function control_handler (way: number) {
+function control_handler(way: number) {
     // how did you even...
     if (!(gen_fin) || gameisdone || target == 0) {
         return
@@ -37,7 +31,7 @@ function control_handler (way: number) {
     if (way == goals[current_spot]) {
         console.log([1, way, goals[current_spot]])
         sprite_in_goals[current_spot].setImage(pos_transg[goals[current_spot]])
-info.changeCountdownBy(0.05)
+        info.changeCountdownBy(0.05)
         playerSprite.setImage(pos_trans[way])
         goalSprite.setImage(pos_transg[goals[current_spot]])
         pause(100)
@@ -71,10 +65,10 @@ let way_left = 0
 let way_up = 0
 let allow_row2 = false
 let spawn_delay = 0
-let gameisdone = false
-let current_spot = 0
-let choie = null
 let playerSprite: Sprite = null
+let choie = null
+let current_spot = 0
+let gameisdone = false
 let goals: any[];
 let sprite_in_goals: any[];
 let i: number;
@@ -82,24 +76,32 @@ let chose: any;
 let time_lim: number;
 // 0 = 0, 1 = 100, 2 = 150, 3 = 200, 4 = 250
 spawn_delay = 1
+let spawn_delay_trans = [0, 100, 150, 200, 250]
+
+let True = true // i miss python...
+let False = false // i miss python...
+
+let score: number;
+let pause_t: number;
+
 allow_row2 = true
 way_up = 1
 way_left = 2
 way_down = 3
 way_right = 4
 let possibles = [
-way_up,
-way_left,
-way_down,
-way_right
+    way_up,
+    way_left,
+    way_down,
+    way_right
 ]
-let pos_trans: {[key:number]:Image} = {
+let pos_trans: { [key: number]: Image } = {
     1: assets.image`myImage`,
     3: assets.image`myImage2`,
     2: assets.image`myImage0`,
     4: assets.image`myImage1`
 }
-let pos_transg: {[key:number]:Image} = {
+let pos_transg: { [key: number]: Image } = {
     1: assets.image`goodup`,
     2: assets.image`goodleft`,
     3: assets.image`gooddown`,
@@ -132,11 +134,17 @@ while (true) {
     sprite_in_goals = []
     i = 0
     choie = null
-    if (spawn_delay != 0) {goalSprite.sayText(target, 20000)}
+    if (spawn_delay != 0) {
+        goalSprite.sayText(target, 20000)
+        info.onCountdownEnd(function () { info.showCountdown(false) })
+        info.startCountdown((target * spawn_delay_trans[spawn_delay] + 50) / 1000)
+    }
     while (i < target) {
         chose = Math.pickRandom(possibles)
         console.logValue("gen", chose)
-        if (spawn_delay != 0) {playerSprite.sayText(i + 1)}
+        if (spawn_delay != 0) {
+            playerSprite.sayText(i + 1)
+        }
         goals[i] = chose
         chose_spr = sprites.create(pos_trans[chose], SpriteKind.Projectile)
         sprite_in_goals[i] = chose_spr
@@ -147,26 +155,32 @@ while (true) {
             chose_spr.y = 45
             chose_spr.x = 10 + (i - 8) * 20
         }
-        if (spawn_delay != 0) {pause([0,100,150,200,250][spawn_delay])}
+        if (spawn_delay != 0) {
+            pause(spawn_delay_trans[spawn_delay])
+        }
         i += 1
     }
-    if (spawn_delay != 0) {pause(250)}
+    if (spawn_delay != 0) {
+        pause(260 * (spawn_delay_trans[spawn_delay] / 100))
+    }
     playerSprite.sayText("", 0)
     goalSprite.sayText("", 0)
     time_lim = (target - 3) * 350 + 3000
     if (target > 8) {
         time_lim += (target - 8) * 200
     }
-    scene.setBackgroundColor(16)
+    scene.setBackgroundColor(0)
     gen_fin = true
     info.startCountdown(time_lim / 1000)
-    info.onCountdownEnd(function() {gameisdone = true})
+    info.onCountdownEnd(function () { gameisdone = true })
     info.showCountdown(true)
     pauseUntil(() => gameisdone)
+    score = info.countdown()
     info.showCountdown(false)
     gen_fin = false
     if (current_spot == target) {
         scene.setBackgroundColor(7)
+        info.changeScoreBy(Math.floor(score))
     } else {
         i = 0
         while (i < target) {
